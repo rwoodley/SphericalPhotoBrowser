28744026e312a1d118ed662e3660384e8c94a1bc
Robert Woodley <blwoodley@yahoo.com>
2017-12-20 23:05:15 -0600

    minor

--- reimannShaderEditor.js
+++ reimannShaderEditor.js
@@ -94,13 +94,6 @@ this.reimannUniformsEditor = function(
         if (e.keyCode == 85) {  // u - take a snap.
 
             var cubeCamera = new THREE.CubeCamera( .1, 1000, 4096 );
-            // var mirrorSphereMaterial = new THREE.MeshBasicMaterial( 
-            //     { color: 0xccccff, envMap: cubeCamera.renderTarget, side: THREE.DoubleSide } );
-
-            // var sphereGeom =  new THREE.SphereGeometry( 5, 32, 16 ); // radius, segmentsWidth, segmentsHeight
-            // var mirrorSphere = new THREE.Mesh( sphereGeom, mirrorSphereMaterial );
-            // _scene.add(mirrorSphere);
-            // _renderer.render( _scene, _camera );
 
 
             var equiUnmanaged = new CubemapToEquirectangular( _renderer, false );
